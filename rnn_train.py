1012de05fafc3084e2eb53720a0f4b491504f3ca
Kalab Oster <kalaboster@gmail.com>
2017-07-10 05:43:59 -0700

    Edit by making number of epochs a variable.

--- rnn_train.py
+++ rnn_train.py
@@ -51,6 +51,10 @@ ALPHASIZE = txt.ALPHASIZE
 INTERNALSIZE = 512
 # Number of network layers.
 NLAYERS = 3
+# Number of Epochs
+EPOCH_AMOUNT = 30
+
+
 learning_rate = 0.001  # fixed learning rate
 dropout_pkeep = 0.8    # some dropout
 
@@ -146,7 +150,7 @@ sess.run(init)
 step = 0
 
 # training loop
-for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_epochs=10):
+for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_epochs=EPOCH_AMOUNT):
 
     # train on one minibatch
     feed_dict = {X: x, Y_: y_, Hin: istate, lr: learning_rate, pkeep: dropout_pkeep, batchsize: BATCHSIZE}
