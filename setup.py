9d2ecaf2bac4c0fbd017755f64540fa1b66229c8
Kalab Oster <kalaboster@gmail.com>
2017-11-27 18:25:42 -0800

    Write the setup files to define the app and define style cfg thought I need to add flak8 to the requirements on a branch.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='wordsum-model-syntax',
+    version='0.0.99',
+    url='',
+    license='Open Source',
+    author='Kalab J. Oster',
+    author_email='',
+    description='Training a model of syntax.'
+)
